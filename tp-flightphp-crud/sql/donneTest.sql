cb3efe4fd1de1776a71bccabaecc3e0e3628f551
Luckas <mihajarabe832@gmail.com>
2025-07-07 09:14:43 +0100

    Create donneTest.sql

---
+++ tp-flightphp-crud/sql/donneTest.sql
@@ -0,0 +1,6 @@
+INSERT INTO etudiant (nom, prenom, email, age) VALUES
+('Rakoto', 'Jean', 'jean.rakoto@example.com', 21),
+('Rasoa', 'Marie', 'marie.rasoa@example.com', 22),
+('Andrianina', 'Paul', 'paul.andrianina@example.com', 20),
+('Solo', 'Nina', 'nina.solo@example.com', 23),
+('Rajaonarison', 'Lala', 'lala.rajaonarison@example.com', 19);
